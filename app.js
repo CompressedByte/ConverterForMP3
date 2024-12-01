7fde9ea0605e7848aa0382c5a5873d67b67350e1
CompressedByte <adrianfqe511@gmail.com>
2024-12-01 17:21:24 -0500

    noveno commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));  // Esto asegura que Express busque en la carpeta correcta
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
